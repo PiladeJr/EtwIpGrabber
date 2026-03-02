9c8fdc401d23f028580f80be9065919463a76e02
pilade <piladejr.tomassini@studenti.unicam.it>
2026-03-02 23:40:32 +0100

    i forgot this one

--- TcpLifeCycleReconstruction/Finalization/TcpConnectionFinalizer.cs
+++ TcpLifeCycleReconstruction/Finalization/TcpConnectionFinalizer.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using EtwIpGrabber.TcpLifeCycleReconstruction.Abstractions;
+using EtwIpGrabber.TcpLifeCycleReconstruction.Models;
 
 namespace EtwIpGrabber.TcpLifeCycleReconstruction.Finalization
 {
-    internal class TcpConnectionFinalizer
+    internal sealed class TcpConnectionFinalizer
+        : ITcpConnectionFinalizer
     {
+        private readonly ICommunityIdProvider _communityId;
+
+        public TcpConnectionFinalizer(
+            ICommunityIdProvider communityId)
+        {
+            _communityId = communityId;
+        }
+
+        public TcpConnectionLifecycle Finalize(
+            TcpFlowInstance flow)
+        {
+            var startAt = flow.FirstSeenUtc;
+
+            var endAt = flow.EndUtc ?? flow.LastSeenUtc;
+
+            if (endAt < startAt)
+                endAt = startAt;
+
+            var duration = endAt - startAt;
+
+            var communityKey = new TcpCommunityIdKey(
+                flow.Key.LocalIp,
+                flow.Key.LocalPort,
+                flow.Key.RemoteIp,
+                flow.Key.RemotePort);
+
+            var communityId =
+                _communityId.Compute(communityKey);
+
+            return new TcpConnectionLifecycle
+            {
+                ProcessId = flow.Key.ProcessId,
+                ProcessName = string.Empty, // opzionale, se disponibile
+
+                LocalIP = flow.Key.LocalIp,
+                LocalPort = flow.Key.LocalPort,
+
+                RemoteIP = flow.Key.RemoteIp,
+                RemotePort = flow.Key.RemotePort,
+
+                StartAt = startAt,
+                EndAt = endAt,
+                Duration = duration,
+
+                CommunityId = communityId
+            };
+        }
     }
 }
